0a1ab5cbe39bf4142ba217ab664a2607e9c2f77c
sheiv <sheiv@PC0360.GT.local>
2017-08-10 14:14:57 +0200

    JPA works and create the EBike database rest call works

--- src/main/java/com/gigatronik/ebikediagnose/controllers/MainController.java
+++ src/main/java/com/gigatronik/ebikediagnose/controllers/MainController.java
@@ -5,46 +5,54 @@
  */
 package com.gigatronik.ebikediagnose.controllers;
 
-import com.gigatronik.ebikediagnose.model.PersonRepository;
+import com.gigatronik.ebikediagnose.model.EBike;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import com.gigatronik.ebikediagnose.model.EBikeRepository;
+import org.springframework.web.client.RestTemplate;
 
 
 /**
  *
  * @author sheiv
  */
-@RestController
+@Controller
 public class MainController {
     
     @Autowired
-    private PersonRepository personRepository;
+    private EBikeRepository bikeRepository;
     
-    @GetMapping("/")
-    public String test(){
+    @RequestMapping(value="/",method = RequestMethod.GET)
+    public String test(Map<String,Object> model){
         
-    return "hello world!";
+        model.put("message", "this is the message");
+        return "index";
     
     }
     
-    
-    @GetMapping(value="/hello")
-    public String storeData(HttpServletRequest request , Model model){
-    
-    String name = request.getParameter("name");
-    
-    if(name == null || name == ""){
-        name = "world";
-    }
-    
-    model.addAttribute("firstName", name);
-    return "hello.html";
+    /** URL will be passed as a parameter from the user interface and dynamically can call the rest api in this method
+     * 
+     * @param request
+     * @param model
+     * @return 
+     */
+    @RequestMapping(value="/text",method = RequestMethod.GET)
+    public String storeData(HttpServletRequest request, Model model){
+
+        
+            String name = request.getParameter("ipAddress");    
+            RestTemplate restTemplate = new RestTemplate();
+            EBike bike = new EBike( (restTemplate.getForObject("http://localhost:8088/ecus/display", EBike.class)).toString());
+            bikeRepository.save(bike);
+            model.addAttribute("name", bike);
+            
+            
+                 return "hello";
     }
     
 }
